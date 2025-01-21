e7205fe52d4e8ea83ddc85f8a043b64fdbab7829
rigdoge <rigdoge@gmail.com>
2025-01-21 11:52:48 +0800

    fix: configure app keys for strapi server

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -2,9 +2,9 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['PapaPanel1', 'PapaPanel2', 'PapaPanel3', 'PapaPanel4'])
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', true),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
